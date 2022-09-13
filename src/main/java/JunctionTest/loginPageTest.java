c1d169d0d8564d3b39930f219bbd4041cd563977
srigowri98 <gowri6498@gmail.com>
2022-09-13 15:13:14 +0530

    first commit from newworkspace from tester1

---
+++ src/main/java/JunctionTest/loginPageTest.java
@@ -0,0 +1,8 @@
+package JunctionTest;
+
+public class loginPageTest {
+	public static void main(String[] args) {
+		System.out.println("first login page displayed");
+	}
+
+}
---
+++ src/test/java/contactTest/LoginPageTest.java
@@ -0,0 +1,9 @@
+package contactTest;
+
+public class LoginPageTest {
+	
+	public static void main(String[] args) {
+		System.out.println("login page one is displayed");
+	}
+
+}
