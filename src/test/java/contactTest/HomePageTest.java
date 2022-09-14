f15b9988b747cc5ab768defcee8dcb9677ba3345
srigowri98 <gowri6498@gmail.com>
2022-09-14 12:12:09 +0530

    3re main page

--- src/test/java/contactTest/HomePageTest.java
+++ src/test/java/contactTest/HomePageTest.java
@@ -4,6 +4,7 @@ public class HomePageTest {
 	public static void main(String[] args) {
 		System.out.println("main page 1 is dispalyed");
 		System.out.println("mai page 2 is displyed");
+		System.out.println("main page 3 ");
 	}
 
 }
