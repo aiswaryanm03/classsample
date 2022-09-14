9295748284f25ecf61e48c458690c1dff2db80b4
srigowri98 <gowri6498@gmail.com>
2022-09-14 11:59:52 +0530

    main page 2

--- src/test/java/contactTest/HomePageTest.java
+++ src/test/java/contactTest/HomePageTest.java
@@ -3,6 +3,7 @@ package contactTest;
 public class HomePageTest {
 	public static void main(String[] args) {
 		System.out.println("main page 1 is dispalyed");
+		System.out.println("mai page 2 is displyed");
 	}
 
 }
--- src/test/java/contactTest/LoginPageTest.java
+++ src/test/java/contactTest/LoginPageTest.java
@@ -4,6 +4,7 @@ public class LoginPageTest {
 	
 	public static void main(String[] args) {
 		System.out.println("login page one is displayed");
+		System.out.println("");
 	}
 
 }
